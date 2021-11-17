a6c9bba7bc8eb90cf6c1ed5dafa4db55b5aa2902
Sai Tejas Janjur <74515547+saitejas-janjur@users.noreply.github.com>
2021-11-16 20:44:27 -0600

    created users schema

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const useRoute = require("./routes/users")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -17,7 +18,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", useRoute)
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800,()=>{
     console.log("Backend is running  dhdht ht hfh");
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+
+router.get("/", (req, res)=>{
+    res.send("Hey, its auth route")
+})
+
+
+module.exports = router
