76db0e85cb0ec5c7e3d99bc3a361b0b2fe378041
Sai Tejas Janjur <74515547+saitejas-janjur@users.noreply.github.com>
2021-11-16 19:29:47 -0600

    created homepage and user function

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ mongoose.connect(process.env.MONGO_ULR, {useNewUrlParser: true, useUnifiedTopolo
     console.log("Connected to MongoDB")
 });
 
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req, res)=>{
+    res.send("welcome to homepage")
+})
+
+app.get("/users",(req, res)=>{
+    res.send("welcome to userpage")
+})
+
+
 app.listen(8800,()=>{
     console.log("Backend is running  dhdht ht hfh")
 })
