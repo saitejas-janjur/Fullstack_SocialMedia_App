6c183e8503de72097bd099fbaf319678948efac7
Sai Tejas Janjur <74515547+saitejas-janjur@users.noreply.github.com>
2021-11-22 20:13:55 -0600

    del unwanted files

--- routes/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-
-router.get("/", (req, res)=>{
-    res.send("Hey, its auth route")
-})
-
-
-module.exports = router
--- routes/routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-
-router.get("/", (req, res)=>{
-    res.send("Hey, its users route")
-})
-
-
-module.exports = router
